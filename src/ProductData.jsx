6dfe041bc69b9711c902f6c5235b8c261da8fb92
Båmi Lee <swedenebomi@gmail.com>
2022-05-18 13:34:40 +0200

    Search 1
    Signed-off-by: Båmi Lee <swedenebomi@gmail.com>

---
+++ src/ProductData.jsx
@@ -0,0 +1,28 @@
+import './imgages/2.jpg';
+import './imgages/3.jpg';
+import './imgages/4.jpg';
+
+const data = {
+    productData: [
+        {
+            id:2,
+            img: 2,
+            name: 'Jensen Royal Kaffe 100 Gram Limited Edition'
+            pris: 500 sek
+        }
+        ,
+        {
+            id:3,
+            img: 3,
+            name: 'Drömmen om Jensen Vitamin D 70 Kaps'
+            pris: 420 sek
+        }
+        ,
+        {
+            id:4,
+            img: 4,
+            name: 'Aladin flyggande multi vitaminer 50 Kaps'
+            pris: 250 sek
+        }
+    ]
+}
