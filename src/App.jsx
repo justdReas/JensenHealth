565b368e5cb3e5d65cb88fa607e76b5c35a2dba5
Andreas Benckert <andreas.benckert@gmail.com>
2022-05-18 14:04:22 +0200

    Removed Shopping Cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,31 @@
-import { useEffect } from 'react';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import { useStates } from './utilities/states';
-import { factory } from './utilities/FetchHelper';
-import { init } from './utilities/shoppingCartLogic';
-import './utilities/scrollBehavior';
+import { useStates } from "./utilities/states";
+import { factory } from "./utilities/FetchHelper";
+import { init } from "./utilities/shoppingCartLogic";
+import "./utilities/scrollBehavior";
 
-import MainNav from './MainNav';
-import StartPage from './StartPage';
-import ProductList from './ProductList';
-import ProductDetail from './ProductDetail';
-import ProductEdit from './ProductEdit';
-import ShoppingCart from './ShoppingCart';
+import MainNav from "./MainNav";
+import StartPage from "./StartPage";
+import ProductList from "./ProductList";
+import ProductDetail from "./ProductDetail";
+import ProductEdit from "./ProductEdit";
 
-import Search from './Search'
+import Search from "./Search";
 
-import ShoppingCart from './ShoppingCart'
-import BackOffice from './BackOffice'
+import ShoppingCart from "./ShoppingCart";
+import BackOffice from "./BackOffice";
 
 // Create classes used for fetching from the REST-api
 const { Product, Categorie: Category } = factory;
 
 export default function App() {
-
-  let s = useStates('main', {
+  let s = useStates("main", {
     products: [],
     categories: [],
     chosenCategoryId: 0,
-    cartContents: []
+    cartContents: [],
   });
 
   useEffect(() => {
@@ -42,11 +36,11 @@ export default function App() {
       s.products = await Product.find();
       // initilize the shopping cart
       // (this provides local storage of cartContents)
-      init(s, 'cartContents');
+      init(s, "cartContents");
     })();
   }, []);
 
-  return s.products.length === 0 ? null :
+  return s.products.length === 0 ? null : (
     <Router>
       <MainNav />
       <Routes>
@@ -55,9 +49,10 @@ export default function App() {
         <Route path="/product-detail/:id" element={<ProductDetail />} />
         <Route path="/BackOffice/:id" element={<ProductEdit />} />
         <Route path="/shopping-cart" element={<ShoppingCart />} />
-    
-      <Route path="/search" element={<Search />} />
+
+        <Route path="/search" element={<Search />} />
         <Route path="/BackOffice" element={<BackOffice />} />
       </Routes>
     </Router>
+  );
 }
