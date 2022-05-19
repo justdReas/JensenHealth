5796fe86294774f71fac845b640a1fb8903326e3
Andreas Benckert <andreas.benckert@gmail.com>
2022-05-19 15:05:25 +0200

    Adjust navbar

--- src/MainNav.jsx
+++ src/MainNav.jsx
@@ -8,20 +8,22 @@ import {
 } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
+import SearchBar from "./SearchBar";
+
 export default function MainNav() {
   // React Router DOM v6 got rid of the NavLink component
   // What is the smartest way to recreate that functionality
   // (adding an active CSS class to the active menu choice)
 
   return (
-    <Navbar bg="dark" variant="dark" expand="lg" className="mb-4">
+    <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand href="/">
+        <Navbar.Brand href="/" className="col-md-4">
           <h1>🤗Jensen Health 🏥 </h1>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
+        <Navbar.Collapse id="basic-navbar-nav" className="col-md-8">
+          <Nav className="ms-auto align-items-center text-center">
             <Link className="nav-link" to="/">
               Hem
             </Link>
@@ -32,23 +34,16 @@ export default function MainNav() {
               <i class="fa fa-cart-arrow-down blue-color"></i>
               Inkorg
             </Link>
-            <Link className="nav-link" to="/search">
+            {/* <Link className="nav-link" to="/search">
               Sök_Produkter
-            </Link>
+            </Link> */}
 
-            <InputGroup className="mb-3">
-              <Button variant="outline-secondary" id="button-addon1">
-                Search<kbd></kbd>
-              </Button>
-              <FormControl
-                aria-label="Example text with button addon"
-                aria-describedby="basic-addon1"
-              />
-            </InputGroup>
+            <SearchBar />
+
             <Nav.Link href="#deets">Om Oss</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
+            {/* <Nav.Link eventKey={2} href="#memes">
               FnQ
-            </Nav.Link>
+            </Nav.Link> */}
           </Nav>
         </Navbar.Collapse>
       </Container>
