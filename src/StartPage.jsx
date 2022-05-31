2ae6a84a069f3293e2d389f31d48e29c503c5a27
Andreas Benckert <andreas.benckert@gmail.com>
2022-05-31 13:32:07 +0200

    Update

--- src/StartPage.jsx
+++ src/StartPage.jsx
@@ -1,6 +1,7 @@
 import imgA from "./img/mo.jpg";
 import imgB from "./img/en.jpg";
 import { Link } from "react-router-dom";
+import { Container, Row, Col } from "react-bootstrap";
 
 export default function StartPage() {
   const home = "Välkommen! ";
@@ -8,29 +9,34 @@ export default function StartPage() {
 
   if (showHome) {
     return (
-      <div className="home">
-        <Link className="text-decoration-none" to="/produkt-lista">
-          <h3>StartSida</h3>
-          <h1>{home}</h1>
-          <h2>
-            {" "}
-            Svensk Hälsokost & speciella vitaminer för IT- studenter och
-            utvecklare!
-          </h2>
-          <img
-            style={{ marginTop: "100px" }}
-            src={imgA}
-            width="500px"
-            height="350px"
-          ></img>
-          <img
-            style={{ marginLeft: "20px", marginTop: "100px" }}
-            src={imgB}
-            width="500px"
-            height="350px"
-          ></img>
-        </Link>
-      </div>
+      <Container className="StartPage">
+        <Row>
+          <Link className="text-decoration-none" to="/produkt-lista">
+            <h1>{home}</h1>
+            <h2>
+              {" "}
+              Svensk Hälsokost & speciella vitaminer för IT- studenter och
+              utvecklare!
+            </h2>
+            <Row className="pb-5">
+              <Col>
+                <img
+                  src={imgA}
+                  width="80%"
+                  // height="350px"
+                ></img>
+              </Col>
+              <Col>
+                <img
+                  src={imgB}
+                  width="80%"
+                  // height="350px"
+                ></img>
+              </Col>
+            </Row>
+          </Link>
+        </Row>
+      </Container>
     );
   } else {
     return <p>empty</p>;
