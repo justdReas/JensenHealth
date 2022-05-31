a2fe055fb8fcac6514a25cd9b3d04c15575b3864
Båmi Lee <swedenebomi@gmail.com>
2022-05-31 11:48:10 +0200

    how to solve for save function?
    Signed-off-by: Båmi Lee <swedenebomi@gmail.com>

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,32 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "../scss/style.scss";
-import App from "./App";
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import "../scss/style.scss";
+// import App from "./App";
 
-ReactDOM.createRoot(document.querySelector("#root")).render(
+// ReactDOM.createRoot(document.querySelector("#root")).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import "../scss/style.scss";
+import App from './App';
+
+// Check if in prodcution (not dev server running on port 3000)
+let inProduction = !location.host.includes(':3000');
+
+// If in production and the browser support service workers
+// then register the service-worker
+if (inProduction && 'serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/serviceWorker.js')
+    .then(reg => console.log('service worker registered', reg))
+    .catch(err => console.log('service worker not registered', err));
+}
+
+ReactDOM.createRoot(document.querySelector('#root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
